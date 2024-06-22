cfee708c2d1a4a5731eacb6aab23109117ee25d2
techie-ranith <ranith2k3@gmail.com>
2024-06-22 10:33:10 +0530

    job description

--- client/src/app/(pages)/(dashboard)/employee/jobs/page.tsx
+++ client/src/app/(pages)/(dashboard)/employee/jobs/page.tsx
@@ -5,7 +5,7 @@ import JobDescription from '@/components/layouts/jobDescription'
 const page = () => {
   return (
     <div className='flex justify-center items-start gap-4'>
-      <div className='flex flex-col items-center justify-center gap-4 overflow-y-auto h-[600px]'>
+      <div className='flex flex-col items-center justify-center gap-4 overflow-y-auto h-[600px] p-2 pt-72'>
         <JobCardSecondary />
         <JobCardSecondary />
         <JobCardSecondary />
