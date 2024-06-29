cf11883ccaf223b522a26b4d93b97e5512beacfb
techie-ranith <ranith2k3@gmail.com>
2024-06-29 09:49:51 +0530

    landing page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -4,20 +4,20 @@ import Hero from "@/components/layouts/Hero";
 import JobCardSection from '@/components/layouts/LandingPage/JobCardSection';
 import Features from '@/components/layouts/LandingPage/Features';
 import Testimonials from '@/components/layouts/LandingPage/Testimonials';
-import QandA from '@/components/layouts/LandingPage/QandA';
+import FAQ from '@/components/layouts/LandingPage/FAQ';
 import Footer from '@/components/layouts/Footer';
 import Container from '@/components/layouts/Container';
 
 const page = () => {
   return (
     <Container>
-      <div className='flex flex-col items-center justify-center border-2 min-w-full'>
+      <div className='flex flex-col items-center justify-center border-2 min-w-full gap-4'>
         <Navbar></Navbar>
         <Hero></Hero>
         <JobCardSection></JobCardSection>
         <Features></Features>
         <Testimonials></Testimonials>
-        <QandA></QandA>
+        <FAQ></FAQ>
         <Footer></Footer>
       </div>
     </Container>
--- client/src/components/layouts/Footer.tsx
+++ client/src/components/layouts/Footer.tsx
@@ -1,10 +1,47 @@
 import React from 'react'
+import Image from 'next/image'
 
-const Footer = () => {
+import logo from '../../../public/logo.png'
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+type Props = {}
+
+const Footer = (props: Props) => {
   return (
-    <div>
-      Footer
-    </div>
+      <div className="flex flex-col sm:flex-row items-center justify-center p-6 font-inter gap-16">
+        <div className="flex flex-col justify-center sm:items-start items-center gap-6 flex-1">
+          <div className="flex flex-col items-center justify-center">
+            <Image src={logo} alt="balck logo" className='' width={100}></Image>
+          </div>
+          <div className="sm:w-2/3 sm:text-left text-justify">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et
+            est leo.
+          </div>
+        </div>
+        <div className="flex flex-col flex-1 items-center justify-center gap-4">
+          <div>
+            <a href="">Home</a>
+          </div>
+          <div>
+            <a href="">Episodes</a>
+          </div>
+          <div>
+            <a href="">About</a>
+          </div>
+        </div>
+        <div className="flex flex-col flex-1 sm:items-end items-center justify-center gap-4">
+          {/* <div>
+            <Image src={logo} alt="foss_logo"></Image>
+          </div> */}
+          <div>Follow us on</div>
+          <div className="flex gap-2">
+            <LinkedInIcon></LinkedInIcon>
+            <InstagramIcon></InstagramIcon>
+            <FacebookIcon></FacebookIcon>
+          </div>
+        </div>
+      </div>
   )
 }
 
