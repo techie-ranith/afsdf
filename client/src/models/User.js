3b714eab0bb832f4939c23914688f081d5aa91f0
yohanmanohara <warushayohan80@gmail.com>
2024-07-08 19:06:06 +0530

    #

--- client/src/models/User.js
+++ client/src/models/User.js
@@ -1,25 +1,44 @@
+import exp from "constants";
 import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
 const userSchema = new Schema(
+  
   {
+  
+
     name: {
       type: String,
       required: true,
     },
     email: {
       type: String,
-      
       required: true,
     },
     password: {
       type: String,
       required: false,
     },
-    verificationCodeExpires: { type: Date, expires: 0, default: Date.now },
+    verificationCode: {
+      type: String,
+      required: false,
+      expires: 30,
+    },
+    
+    token_id:{
+      type: String,
+      required: false,
+    },
+
+
+    
+
   },
-  { timestamps: true ,collection: "user"}
+
+  { timestamps: true, collection: "user" }
 );
 
 export default mongoose.models.User || mongoose.model("User", userSchema);
+
+
