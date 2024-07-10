ac8c27f0cdb0b4dfe54b421c7a2d509e090c6f2b
yohanmanohara <warushayohan80@gmail.com>
2024-07-10 14:03:36 +0530

    #

--- client/src/app/(pages)/(dashboard)/employee/settings/profile/page.tsx
+++ client/src/app/(pages)/(dashboard)/employee/settings/profile/page.tsx
@@ -34,69 +34,16 @@ import { useSession } from 'next-auth/react';
 import Company from '@/components/layouts/profile/company';
 import User from '@/models/User';
 
-
-
-export default function MyProfile() {
-
-  return (<>
-  <Box
-        sx={{
-          position: 'sticky',
-          top: { sm: -100, md: -110 },
-          bgcolor: 'background.body',
-          zIndex: 9995,
-        }}
-      >
- 
-        <Box sx={{ px: { xs: 2, md: 6 }}}>
-          <Breadcrumbs
-            size="sm"
-            aria-label="breadcrumbs"
-            sx={{ pl: 0 }}
-          >
-            <Link
-              underline="none"
-              color="neutral"
-              href="#some-link"
-              aria-label="Home"
-            >
-              <HomeRoundedIcon />
-            </Link>
-            <Link
-              underline="hover"
-              color="neutral"
-              href="#some-link"
-              fontSize={12}
-              fontWeight={500}
-            >
-             Settings
-            </Link>
-            <Typography color="primary" fontWeight={500} fontSize={12}>
-             profile
-            </Typography>
-          </Breadcrumbs>
-          <Typography level="h2" component="h1" sx={{ mt: 1, mb: 2 }}>
-            Company/User  profile
-          </Typography>
-        </Box>
-        
-
-
-      </Box>
+function page() {
+  return (
+    <>
     
-
-
-    <div className=' flex flex-row'>
-
-      <Company/>
-      <Userprofile/>
-
-      </div>
-
-
-
-
-   
+    <div>
+        <Userprofile/>
+      
+    </div>
     </>
-  );
+  )
 }
+
+export default page
